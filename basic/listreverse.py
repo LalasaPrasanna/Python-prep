b9563a8e4d9757f4819ea504d43a6b521acd3796
LalasaPrasanna <167599797+LalasaPrasanna@users.noreply.github.com>
2026-06-17 16:26:03 +0000

    New Commit

---
+++ basic/listreverse.py
@@ -0,0 +1,9 @@
+def listreverse(arr):
+    l,r = 0, len(arr)-1
+    while l<r:
+        arr[l],arr[r] = arr[r],arr[l]
+        l+=1
+        r-=1
+    return arr
+arr = list(map(int,input().split()))
+print(listreverse(arr))
